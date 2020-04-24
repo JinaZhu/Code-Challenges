7fc3fcf95499c970cfa91b78f0c516de730e9e60
Jina Zhu <jinazhu87@gmail.com>
2020-04-24 09:17:04 -0700

    created the head of the linked list

---
+++ linkedList/singly-linked-list.js
@@ -0,0 +1,12 @@
+// What a linked list looks linked
+// 10-->5-->16
+head: {
+    value: 10,
+    next: {
+        value: 5,
+        next: {
+            value: 16, 
+            next: null
+        }
+    } 
+}

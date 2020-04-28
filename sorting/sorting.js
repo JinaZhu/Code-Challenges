7a883a7ef2b6f2937eedc6324df920535565fa06
Jina Zhu <jinazhu87@gmail.com>
2020-04-27 18:14:33 -0700

    merge sort in js

--- sorting/sorting.js
+++ sorting/sorting.js
@@ -15,3 +15,34 @@ function bubbleSort(arr) {
 }
 
 console.log(bubbleSort(notSorted));
+
+// merge sort
+function mergeSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  const middle = Math.floor(arr.length / 2);
+  const arr1 = mergeSort(arr.slice(0, middle));
+  const arr2 = mergeSort(arr.slice(middle));
+
+  return makeMerge(arr1, arr2);
+}
+
+function makeMerge(arr1, arr2) {
+  const result = [];
+
+  while (arr1.length > 0 || arr2.length > 0) {
+    if (arr1.length === 0) {
+      result.push(arr2.shift());
+    } else if (arr2.length === 0) {
+      result.push(arr1.shift());
+    } else if (arr1[0] < arr2[0]) {
+      result.push(arr1.shift());
+    } else {
+      result.push(arr2.shift());
+    }
+  }
+  return result;
+}
+
+console.log(mergeSort(notSorted));

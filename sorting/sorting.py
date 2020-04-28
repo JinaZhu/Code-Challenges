83929025738eedb57155c70e37af4a4c0882d63e
Jina Zhu <jinazhu87@gmail.com>
2020-04-27 17:35:15 -0700

    bubbly sort

--- sorting/sorting.py
+++ sorting/sorting.py
@@ -8,7 +8,6 @@ def bubblesort(lst):
         for j in range(len(lst)-1):
             if lst[j] > lst[j+1]:
                 lst[j], lst[j+1] = lst[j+1], lst[j]
-    return lst
 
 
 print(bubblesort(notSorted))

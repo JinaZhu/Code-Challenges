0f30a86735d60c46656d819cb7ad11293d7dc32b
Jina Zhu <jinazhu87@gmail.com>
2020-04-28 21:02:20 -0700

    stack with arrays

--- stack.js
+++ stack.js
@@ -28,4 +28,53 @@ class Stack {
     this.length++;
     return this;
   }
+  pop() {
+    if (!this.pop) {
+      return null;
+    }
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    }
+    this.top = this.top.next;
+    this.length--;
+    return this;
+  }
+  isEmpty() {
+    if (this.length === 0) {
+      return true;
+    } else {
+      return this;
+    }
+  }
 }
+
+const myStack = new Stack();
+console.log(myStack.isEmpty());
+console.log(myStack.push("google"));
+myStack.push("Udemy");
+myStack.push("discord");
+myStack.pop();
+myStack.peek();
+
+class stackArray {
+  constructor() {
+    this.array = [];
+  }
+  peek() {
+    return this.array[this.array.length - 1];
+  }
+  push(value) {
+    this.array.push(value);
+    return this;
+  }
+  pop() {
+    this.array.pop();
+    return this;
+  }
+}
+
+const myStack2 = new stackArray();
+myStack2.push(1);
+myStack2.push(2);
+myStack2.push(3);
+myStack2.pop();

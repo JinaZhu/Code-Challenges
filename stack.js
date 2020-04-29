cc98a6baa596bdafb41b7455611ecfedb33e9876
Jina Zhu <jinazhu87@gmail.com>
2020-04-28 19:28:02 -0700

    created stack and added peek and pop

---
+++ stack.js
@@ -0,0 +1,31 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.top;
+  }
+  push(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const holderPointer = this.top;
+      this.top = newNode;
+      this.top.next = holderPointer;
+    }
+    this.length++;
+    return this;
+  }
+}

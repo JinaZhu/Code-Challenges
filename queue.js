66c3a80b55991854bee319eedfed9590a8368dd2
Jina Zhu <jinazhu87@gmail.com>
2020-04-29 12:37:53 -0700

    added queue using list

--- queue.js
+++ queue.js
@@ -53,3 +53,43 @@ myQueue.enqueue("matt");
 myQueue.enqueue("pavel");
 console.log(myQueue.enqueue("Samir"));
 console.log(myQueue.dequeue());
+
+class CrazyQueue {
+  constructor() {
+    this.first = [];
+    this.last = [];
+  }
+  enqueue(value) {
+    const length = this.first.length;
+    for (let i = 0; i < length; i++) {
+      this.last.push(this.first.pop());
+    }
+    this.last.push(value);
+    return this;
+  }
+  dequeue() {
+    const length = this.last.length;
+    for (let i = 0; i < length; i++) {
+      this.first.push(this.last.pop());
+    }
+    this.first.pop();
+    return this;
+  }
+  peek() {
+    if (this.last.length > 0) {
+      return this.last[0];
+    }
+    return this.first[this.first.length - 1];
+  }
+}
+
+const myQueue2 = new CrazyQueue();
+myQueue2.peek();
+myQueue2.enqueue("Joy");
+myQueue2.enqueue("Matt");
+myQueue2.enqueue("Pavel");
+myQueue2.peek();
+myQueue2.dequeue();
+myQueue2.dequeue();
+myQueue2.dequeue();
+myQueue2.peek();

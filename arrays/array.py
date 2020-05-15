136e4c1eef6c2fb8f4660eb61f5e46cea493622b
Jina Zhu <jinazhu87@gmail.com>
2020-05-14 21:43:58 -0700

    added rotate in py

--- arrays/array.py
+++ arrays/array.py
@@ -84,3 +84,21 @@ def plus_one(nums):
 
 print('plus_one', plus_one([1, 2, 3]))
 print('plus_one', plus_one([9, 9, 9]))
+
+# You are given an n x n 2D matrix representing an image. Rotate the image by 90 degrees (clockwise).
+
+
+def rotate(matrix):
+    for i in range(len(matrix)):
+        # j should always start at index i
+        for j in range(i, len(matrix[i])):
+            # swap position
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for row in matrix:
+        row.reverse()
+
+    return matrix
+
+
+print('rotate', rotate([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))

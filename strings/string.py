6cb8754b3a6d0bed7ccc0066e345002b1e23fad4
Jina Zhu <jinazhu87@gmail.com>
2021-03-04 12:21:10 -0800

    add count and say problem from dropbox

--- strings/string.py
+++ strings/string.py
@@ -31,3 +31,34 @@ a = "(abc)d(efg)"
 print(reverse_in_parentheses(a))
 b = "foo(bar)baz"
 print(reverse_in_parentheses(b))
+
+# Count and Say
+# The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+# countAndSay(1) = "1"
+# countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1), which is then converted into a different digit string.
+# https://leetcode.com/problems/count-and-say/
+
+def count_and_say(times):
+    if times == 1:
+        return "1"
+    
+    current = "1"
+
+    for _ in range(times-1):
+        next = ""
+        count = 1
+
+        for j in range(len(current)-1):
+            if current[j] == current[j+1]:
+                count += 1
+            else:
+                next += str(count) + current[j]
+                count = 1
+        next += str(count) + current[-1]
+        current = next
+    return current 
+
+count_and_say_testcase = 7
+count_and_say_result = count_and_say(count_and_say_testcase)
+count_and_say_test = count_and_say_result == "13112221"
+print('count_and_say_test', count_and_say_test)
